b6a06b0ff68f153ab956128a689b472fc87e0c8f
BryanRonad <bryanronad@gmail.com>
2022-12-04 11:42:50 +0530

    feat: fourth day

---
+++ Day 4/part2.py
@@ -0,0 +1,11 @@
+res = list()
+with open("input.txt") as file:
+    input = [[x for x in y.split(",")]
+             for y in file.read().split("\n")]
+    for line in input:
+        first = set(range(int(line[0].split('-')[0]),
+                    int(line[0].split("-")[1])+1))
+        second = set(
+            range(int(line[1].split('-')[0]), int(line[1].split("-")[1])+1))
+        res.append(len(first.intersection(second)) > 0)
+    print(sum(res))
