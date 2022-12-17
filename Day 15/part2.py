ad5098c9df496d20dec728dc27202b8ff16b998c
BryanRonad <bryan.ronad@schbang-q.com>
2022-12-17 21:12:22 +0530

    feat: fifteenth day

--- Day 15/part2.py
+++ Day 15/part2.py
@@ -1,8 +1,20 @@
-row = 2000000
+dimension = 4000000
 
 
 def manhattan_distance(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+
+def get_diamond_points(left, right, up, down):
+    points = []
+    x_mid = (left[0] + right[0]) // 2
+    y_mid = (up[1] + down[1]) // 2
+    radius = abs(x_mid - left[0])
+    for x in range(left[0], right[0] + 1):
+        for y in range(up[1], down[1] + 1):
+            if abs(x-x_mid) + abs(y-y_mid) <= radius:
+                points.append([x, y])
+    return points
 
 
 def is_point_in_diamond(point, diamond):
@@ -18,7 +30,7 @@ def is_point_in_diamond(point, diamond):
     return x_dist / size[0] + y_dist / size[1] <= 1
 
 
-with open("input.txt") as file:
+with open("/Users/bryanronad/Documents/Practice/AdventOfCode-2022/Day 15/input.txt") as file:
     input = file.read().split("\n")
     sensor_arr = list()
     beacon_arr = list()
@@ -36,6 +48,7 @@ with open("input.txt") as file:
     # Check coverage of each sensor
     covered_yet = list()
     diamonds = list()
+    diamond_points = list()
     for sensor in sensor_arr:
         # Get the beacon of the sensor
         beacon = sensor_dict[tuple(sensor)]
@@ -43,22 +56,41 @@ with open("input.txt") as file:
         distance = manhattan_distance(sensor, beacon)
         diamond = ((sensor[0]-distance, sensor[1]), (sensor[0]+distance, sensor[1]),
                    (sensor[0], sensor[1]-distance), (sensor[0], sensor[1]+distance))
+        # diamond_points += get_diamond_points(*diamond)
         diamonds.append(diamond)
-    # Get min and max x coordinate of beacon or sensor
-    # Extract all the x values into a separate list
+
+    # Check for unknown beacon's location
+    # unknown_beacon = tuple()
+    # for i in range(row):
+    #     for j in range(col):
+    #         if [i, j] in diamond_points:
+    #             continue
+    #         unknown_beacon = (i, j)
+
+    # print(4000000*unknown_beacon[0] + unknown_beacon[1])
+
+    # Faster check for unknown beacon's location
+        # Get min and max x coordinate of beacon or sensor
+        # Extract all the x values into a separate list
     x_values = [x for t in diamonds for x, y in t]
 
     # Find the lowest and highest x values
     min_x = min(x_values)
     max_x = max(x_values)
-    # Check the rowth row if exists in diamonds
-    hashes = 0
-    flag = False
-    for i in range(min_x, max_x+1):
-        for diamond_index, diamond in enumerate(diamonds):
-            if is_point_in_diamond((i, row), diamond):
-                flag = True
+
+    unknown_beacon = False
+    flag = True
+    for i in range(dimension+1):
+        for j in range(dimension+1):
+            for diamond in diamonds:
+                if is_point_in_diamond((i, j), diamond):
+                    flag = False
+                    break
+            if flag:
+                unknown_beacon = (i, j)
                 break
-        hashes += 1 if flag and [i, row] not in sensor_arr+beacon_arr else 0
-        flag = False
-    print(hashes)
+            flag = True
+        if unknown_beacon:
+            print(unknown_beacon)
+            print(4000000*unknown_beacon[0] + unknown_beacon[1])
+            break
